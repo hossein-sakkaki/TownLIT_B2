2c5382e0a884efd277844c3f2fc3a98b0e3d774a
Hossein Sakkaki <hosseinsakkaki@Hosseins-iMac.local>
2025-12-05 13:09:04 -0800

    Update: V2.10.4 - Email Campaign (Update again)

--- townlit_b/settings.py
+++ townlit_b/settings.py
@@ -567,7 +567,10 @@ USE_I18N = True
 
 # ✅ Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'townlit_local_static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
