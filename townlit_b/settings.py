8c3675e76ee2d7ef35b075a74fd3b4def4845519
Hossein Sakkaki <sakkaki.hossein@gmail.com>
2026-06-11 07:44:39 -0700

    Accounting App Upgrade-v2.2

--- townlit_b/settings.py
+++ townlit_b/settings.py
@@ -203,7 +203,7 @@ INSTALLED_APPS = [
     
     # "apps.core.streams.apps.StreamsConfig",
     "apps.core.interactions",
-    "apps.core.boundaries.apps.BoundariesConfig",
+    # "apps.core.boundaries.apps.BoundariesConfig",
     
     'apps.asset_delivery.apps.AssetDeliveryConfig',
     "apps.subtitles.apps.SubtitlesConfig",
